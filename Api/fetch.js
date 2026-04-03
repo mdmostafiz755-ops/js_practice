976a7eb8761da77b07301f71d19fa3e2d1680414
mdmostafiz755-ops <mdmostafiz755@gmail.com>
2026-04-03 16:03:13 +0600

    did some console log using api

--- Api/fetch.js
+++ Api/fetch.js
@@ -3,4 +3,23 @@ const loadData = () => {
     fetch("https://jsonplaceholder.typicode.com/todos/1")
         .then((response) => response.json())//ekta promise
         .then((data) => console.log(data));//2nd again promise and get data
+};
+
+
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((post) => {
+            console.log(post);
+            displayPost(post);
+        });
+};
+
+
+const displayPost = (posts) => {
+    posts.forEach((p) => {
+        console.log(p);
+    });
 }
